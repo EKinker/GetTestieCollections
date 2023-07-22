a893ebd68452272aee40108bd85bd90c445de9a1
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-22 11:19:05 -0400

    Stack tests

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -25,12 +25,37 @@ public class TestStack {
     @org.junit.Test
     public void testStackPush(){
         Stack<String> stack = new Stack<>();
-        stack.push("Rabble Rabble");
+        stack.push("Don't panic.");
         int expected = 1;
         int actual = stack.size();
 
         assertEquals(expected, actual);
     }
 
+    @org.junit.Test
+    public void testStackPop(){
+        Stack<String> stack = new Stack<>();
+        String expected = "and been widely regarded as a bad move.";
+        stack.push("In the beginning");
+        stack.push("the universe was created.");
+        stack.push ("This has made a lot people very angry");
+        stack.push(expected);
+
+        String actual =stack.pop(); // takes the last item
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.Test
+    public void testStackIsEmpty(){
+        Stack<String> stack = new Stack<>();
+
+        assertTrue(stack.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testStackPeek(){
+
+    }
     // Make a bigger test exercising more Stack methods.....
 }
